99b6e2419626ef052f1b5808e3b5f7c1d0620a20
Guðmundur Ellert <gudmundur.ellert@gmail.com>
2015-10-14 02:05:47 +0000

    Tests added for new line support

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -27,4 +27,8 @@ public class CalculatorTest {
         public void testMultipleNumbers(){
         	assertEquals(6, Calculator.add("1,2,3"));
         }
+	@Test
+	public void testNewLine(){
+		assertEquals(7, Calculator.add("2\n2,3"));
+	}
 }
