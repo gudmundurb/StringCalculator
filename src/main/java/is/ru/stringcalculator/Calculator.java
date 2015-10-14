b63c49fc89992747afd69250d25133e8bbd28131
Guðmundur Ellert <gudmundur.ellert@gmail.com>
2015-10-14 01:42:09 +0000

    support added for null string and adding numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -4,7 +4,14 @@ public class Calculator {
 
 	public static int add(String text){
 		
-		return 0;
+	
+	if(text.equals("")){
+	    	return 0;
 	}
+	else if(text.contains(",")) {
+		return Integer.parseInt(text[0]) + Integer.parseInt(text[1]) 
+	}
+	else 
+		return -1;
 
 }
