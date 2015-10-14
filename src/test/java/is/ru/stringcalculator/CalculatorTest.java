ba221eeb78d4b8ced5825cc744e83731cb3793ba
Guðmundur Ellert <gudmundur.ellert@gmail.com>
2015-10-14 01:50:15 +0000

    tests added for adding numbers together

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -18,5 +18,12 @@ public class CalculatorTest {
 		assertEquals(1, Calculator.add("1"));	
 	
 	}
+t
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+	}	
 
-}
+	@Test
+        public void testMultipleNumbers(){
+        	assertEquals(6, Calculator.add("1,2,3"));
+        }
