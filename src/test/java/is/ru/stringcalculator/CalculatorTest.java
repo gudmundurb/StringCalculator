7c49b1fa2f877cb6c5dfd6b3956f6f49298bc6bf
Guðmundur Ellert <gudmundur.ellert@gmail.com>
2015-10-17 14:30:43 +0000

    Multiple delimiters allowed Test

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -65,4 +65,9 @@ public class CalculatorTest {
 	
 		 assertEquals(6, Calculator.add("//[***]\n1***2***3"));
 	}
+	@Test
+	public void multipleDelimitersOfAnyLength() {
+	
+		 assertEquals(6, Calculator.add("//[*][%]\n1*2%3"));
+	}
 }
