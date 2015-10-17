8a554fa00c40ac0893e5e0d23d5b648883b5fac2
Guðmundur Ellert <gudmundur.ellert@gmail.com>
2015-10-17 05:09:33 +0000

    More tests added for each @Test

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -16,24 +16,43 @@ public class CalculatorTest {
 	@Test
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));	
+		assertEquals(2, Calculator.add("2"));	
+		assertEquals(3, Calculator.add("3"));	
 	
 	}
 	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
+		assertEquals(8, Calculator.add("3,5"));
+		assertEquals(42, Calculator.add("12,30"));
 	}	
 
 	@Test
         public void testMultipleNumbers(){
         	assertEquals(6, Calculator.add("1,2,3"));
+        	assertEquals(1, Calculator.add("1,0,0"));
+        	assertEquals(42, Calculator.add("11,12,13,6"));
         }
 	@Test
 	public void testNewLine(){
 		assertEquals(7, Calculator.add("2\n2,3"));
+		assertEquals(122, Calculator.add("100\n20,2"));
+		assertEquals(42, Calculator.add("40\n2,0"));
 	}
 	@Test
 	public void testNewDelimiter(){
 		assertEquals(12, Calculator.add("//;\n5;7"));
+		assertEquals(12, Calculator.add("//:\n5:7"));
+		assertEquals(12, Calculator.add("//R\n5R7"));
 	
 	}
+	@Test
+        public void negativeNotAllowed(){
+          try{
+               Calculator.add("-2,3,-4");
+	      // Assert.fail("Should have thrown NegativeNumberException but did not!");
+          }catch (Exception negError){
+         assertEquals("Negative numbers not allowed: -2, -4", negError.getMessage());
+          }
+        }
 }
