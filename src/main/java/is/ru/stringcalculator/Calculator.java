f9bd7e1dabf553d80116dc9f93e90522b43fae43
Guðmundur Ellert <gudmundur.ellert@gmail.com>
2015-10-17 01:39:58 +0000

    Set New Delimitor implementation

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,32 +1,63 @@
 package is.ru.stringcalculator;
-
 public class Calculator {
 
 	public static int add(String text){
-		
-  	if(text.equals("")){
+  	
+	if(text.isEmpty()){
  		return 0;
+	}
+	else if(text.contains("//") && text.contains("\n"))  {
+		
+		return changeDelim(text);
 	}
 	else if(text.contains(",")){
 		return sum(splitNumbers(text));
 	}
 	else
-		return 1;
+		return toInt(text);
 	}
 
 	private static int toInt(String number){
+		if (isInt(number)) {
 		return Integer.parseInt(number);
+		}
+		else {
+		  return 0;
+		}
 	}
 
 	private static String[] splitNumbers(String numbers){
 		return numbers.split("[, | \n]");
     	}
-																									      
+	
         private static int sum(String[] numbers){
  	    int total = 0;
             for(String number : numbers){
+	    	if(isInt(number)){
    		    total += toInt(number);
-   		}
-																																return total;
+		}
+   	    }
+	    return total;
+	}
+	private static Boolean isInt(String numbers){
+		try{
+		  int number = Integer.parseInt(numbers);
+		  }catch(NumberFormatException illegalNumber ){
+		    return false;
+		   }
+		return true;
+	}
+	private static int changeDelim(String numbers) {
+	  String delimiter = "";
+	  delimiter = Character.toString(numbers.charAt(2));
+	  numbers = numbers.replace(delimiter, ",");
+	  return newLine(numbers);
+
+	}
+	private static int newLine(String numbers) {
+		numbers = numbers.replace("\n" , ",");
+		return sum(splitNumbers(numbers));
+	
+	
 	}
 }
